500d5bb69e6a4abdeca20d4c32b80b51576d8331
Hossain <fpmirhmd@gmail.com>
2024-10-25 19:25:08 +0600

    added new method gretting

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
 public class Hello{
 public static void main(String[] args){
     System.out.println("Vs code is Awesome");
+
+    gretting("Hossain");
+}
+
+public static void gretting(String name){
+    System.out.println(name);
 }
 
 }
